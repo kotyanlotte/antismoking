f59b5b10d720bcf587a676f8d649bd1546101190
kotyan0427 <koulotte0427@outlook.jp>
2021-08-03 09:48:47 +0900

    eslintとprettierの導入

--- resources/src/components/index.tsx
+++ resources/src/components/index.tsx
@@ -1,16 +1,10 @@
-import React, { VFC } from 'react';
-import ReactDOM  from 'react-dom';
+import React, { VFC } from "react";
+import ReactDOM from "react-dom";
 
-const Index:VFC = () => {
-
-  return (
-    <h1 className='text-red-600'>Hello World</h1>
-  )
+const Index: VFC = () => {
+    return <h1 className="text-red-600">Hello World</h1>;
 };
 
 export default Index;
 
-ReactDOM.render(
-  <Index />,
-  document.getElementById('app')
-)
+ReactDOM.render(<Index />, document.getElementById("app"));
