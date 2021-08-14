a9c2ad1f906e6013e2fb9818d4c8aebd60301098
kotyan0427 <koulotte0427@outlook.jp>
2021-08-14 11:48:24 +0900

    headerの作成

---
+++ resources/src/components/organisms/header.tsx
@@ -0,0 +1,37 @@
+import React, { VFC } from "react";
+import { Link } from "react-router-dom";
+
+import { PrimaryButton } from "@/components/atoms/buttons/primaryButton";
+
+export const Header: VFC = () => {
+    return (
+        <header className="h-20 w-11/12 mx-auto flex flex-col items-center pt-2 surface:flex-row surface:items-center surface:pt-0 surface:justify-between">
+            <Link to="about">
+                <span className="text-4xl md:text-5xl">Anti</span>
+                <span className="text-4xl text-green-300 md:text-5xl">
+                    Smoking
+                </span>
+            </Link>
+            <nav>
+                <ul className="flex space-x-3 mt-2">
+                    <Link to="/signin">
+                        <PrimaryButton
+                            style={"bg-green-300 text-white hover:bg-green-400"}
+                        >
+                            新規登録
+                        </PrimaryButton>
+                    </Link>
+                    <Link to="/signup">
+                        <PrimaryButton
+                            style={
+                                "text-green-300 border-green-300 hover:bg-green-300 hover:text-white"
+                            }
+                        >
+                            ログイン
+                        </PrimaryButton>
+                    </Link>
+                </ul>
+            </nav>
+        </header>
+    );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
                 body: ["Kameron", "Noto Sans JP", "sans-serif"],
             },
         },
+        screens: {
+            surface: "540px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024ppx",
+            xl: "1280px",
+        },
     },
     variants: {
         extend: {},
