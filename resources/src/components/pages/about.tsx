3c5e1cc38986cce8e31b5e892e248f5122bc9877
kotyan0427 <koulotte0427@outlook.jp>
2021-08-14 12:00:23 +0900

    Topページの作成

---
+++ resources/src/components/pages/about.tsx
@@ -0,0 +1,18 @@
+import React, { VFC } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Header } from "@/components/organisms/header";
+import { Signin } from "@/components/pages/signin";
+
+export const About: VFC = () => {
+    return (
+        <>
+            <Header />
+            <Router>
+                <Switch>
+                    <Route path="/signin" component={Signin} />
+                </Switch>
+            </Router>
+        </>
+    );
+};
