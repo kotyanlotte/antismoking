0557cfdc1d7ecec11589413a855cf282a31c29da
kotyan0427 <koulotte0427@outlook.jp>
2021-08-16 10:25:22 +0900

    headerのレイアウト修正

--- resources/src/components/organisms/header.tsx
+++ resources/src/components/organisms/header.tsx
@@ -5,7 +5,7 @@ import { PrimaryButton } from "@/components/atoms/buttons/primaryButton";
 
 export const Header: VFC = () => {
     return (
-        <header className="h-20 w-11/12 mx-auto flex flex-col items-center pt-2 surface:flex-row surface:items-center surface:pt-0 surface:justify-between">
+        <header className="h-28 w-11/12 mx-auto flex flex-col items-center pt-2 surface:h-24 surface:flex-row surface:items-center surface:pt-0 surface:justify-between">
             <Link to="about">
                 <span className="text-4xl md:text-5xl">Anti</span>
                 <span className="text-4xl text-green-default md:text-5xl">
