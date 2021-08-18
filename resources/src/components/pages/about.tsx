018b8ab2ab7e26b95104d869fb066f6bfc88cab2
kotyan0427 <koulotte0427@outlook.jp>
2021-08-18 21:23:42 +0900

    ルーティングの修正

--- resources/src/components/pages/about.tsx
+++ resources/src/components/pages/about.tsx
@@ -1,20 +1,13 @@
 import React, { VFC } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Card } from "@/components/organisms/card";
 import { FirstViewImage } from "@/components/organisms/firstViewImage";
 import { Header } from "@/components/organisms/header";
-import { Signin } from "@/components/pages/signin";
 
 export const About: VFC = () => {
     return (
         <>
             <Header />
-            <Router>
-                <Switch>
-                    <Route path="/signin" component={Signin} />
-                </Switch>
-            </Router>
             <FirstViewImage />
             <Card />
         </>
