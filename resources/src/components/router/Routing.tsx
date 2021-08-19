0051e1dabbe27eb23329f62181fef2598553e100
kotyan0427 <koulotte0427@outlook.jp>
2021-08-19 16:27:11 +0900

    ファイル名の変更

--- resources/src/components/router/Routing.tsx
+++ resources/src/components/router/Routing.tsx
@@ -8,8 +8,10 @@ import {
 } from "react-router-dom";
 
 import { About } from "@/components/pages/About";
+import { Privacy } from "@/components/pages/Privacy";
 import { Signin } from "@/components/pages/signin";
 import { Signup } from "@/components/pages/signup";
+import { Terms } from "@/components/pages/Terms";
 
 export const Routing: VFC = () => {
     return (
@@ -19,6 +21,8 @@ export const Routing: VFC = () => {
                 <Route path="/about" component={About} />
                 <Route path="/signin" component={Signin} />
                 <Route path="/signup" component={Signup} />
+                <Route path="/terms" component={Terms} />
+                <Route path="/privacy" component={Privacy} />
             </Switch>
         </Router>
     );
