d232af0b3c955aaadcdcc6c8274b09b1ff6f767e
kotyan0427 <koulotte0427@outlook.jp>
2021-08-19 10:33:56 +0900

    テストログイン用ボタンの作成

--- resources/src/components/organisms/firstViewImage.tsx
+++ resources/src/components/organisms/firstViewImage.tsx
@@ -1,5 +1,6 @@
 import React, { VFC } from "react";
 
+import { PrimaryButton } from "@/components/atoms/buttons/PrimaryButton";
 import { Picture } from "@/components/molecules/Picture";
 import { image } from "@/components/utils/image";
 
@@ -18,6 +19,9 @@ export const FirstViewImage: VFC = () => {
             <p className="absolute top-40 left-5 py-2 px-1 tracking-wider w-3/4 text-xl text-white-default font-bold bg-black-dark bg-opacity-20 max-w-320 surface:top-36">
                 「AntiSmoking」はタバコが健康に与える影響を数値化します
             </p>
+            <PrimaryButton style="absolute top-72 left-1/2 -translate-x-1/2 text-green-default bg-white-default border-green-default">
+                テストログイン
+            </PrimaryButton>
         </div>
     );
 };
