9ad572b80c99eb2b1406733db997ec1007905ed7
kotyan0427 <koulotte0427@outlook.jp>
2021-08-19 13:31:13 +0900

    TermsページとPrivacyページのルーティング追加

--- resources/src/components/pages/Privacy.tsx
+++ resources/src/components/pages/Privacy.tsx
@@ -1,9 +1,9 @@
 import React, { VFC } from "react";
 
-export const Policy: VFC = () => {
+export const Privacy: VFC = () => {
     return (
         <>
-            <h1>policy</h1>
+            <h1>Privacy</h1>
         </>
     );
 };
