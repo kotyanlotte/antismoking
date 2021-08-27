96da579520ddec5fccccda469d6df4bc78e66d95
kotyan0427 <koulotte0427@outlook.jp>
2021-08-27 17:51:42 +0900

    usersテーブルのカラムの変更

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,17 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements("id");
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string("password_confirmation");
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
+/**
      * Reverse the migrations.
      *
      * @return void
