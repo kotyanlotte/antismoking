c67e4ae152c3d6beb4322ae0c04afe32835c8b0c
kotyan0427 <koulotte0427@outlook.jp>
2021-08-31 13:17:23 +0900

    spaを使用しているのでviewの削除

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -31,10 +31,6 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Fortify::registerView(function () {
-            return view('welcome');
-        });
-
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
