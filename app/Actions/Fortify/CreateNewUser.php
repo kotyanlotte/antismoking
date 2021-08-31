b77c224d4cad7073220687122aa0d188346e2b4b
kotyan0427 <koulotte0427@outlook.jp>
2021-08-31 13:16:56 +0900

    password_confimationがinsert出来ないエラーの修正

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -36,6 +36,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'password_confirmation' => Hash::make($input['password_confirmation'])
         ]);
     }
 }
