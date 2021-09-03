918cda9555947da925e1087486f7e10c5ad34a0a
kotyan0427 <koulotte0427@outlook.jp>
2021-09-03 12:02:24 +0900

    アカウント登録する関数の修正

--- resources/src/components/organisms/RegisterArea.tsx
+++ resources/src/components/organisms/RegisterArea.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, VFC } from "react";
+import React, { VFC } from "react";
 import { Link } from "react-router-dom";
 
 import { PrimaryButton } from "@/components/atoms/buttons/PrimaryButton";
@@ -7,11 +7,6 @@ import { RegisterForm } from "@/components/organisms/forms/RegisterForm";
 
 export const RegisterArea: VFC = () => {
     const { register, loading } = useRegister();
-
-    // ユーザーを登録する関数
-    const onClickRegister = useCallback(() => {
-        register();
-    }, [register]);
 
     return (
         <div className="bg-gray-light py-12 flex justify-center">
@@ -25,7 +20,7 @@ export const RegisterArea: VFC = () => {
                 <PrimaryButton
                     loading={loading}
                     style="text-green-default w-52 border-green-default hover:bg-green-default hover:text-white-default"
-                    onClick={onClickRegister}
+                    onClick={register}
                     id={"register"}
                 >
                     登録
