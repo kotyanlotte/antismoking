298f8d3d851d5b1d55ce995b7cf19bafa4127d48
kotyan0427 <koulotte0427@outlook.jp>
2021-09-18 20:22:09 +0900

    /forgot-passwordエンドポイントに送信するフィールドをemailに変更

--- resources/src/components/organisms/forms/ForgotPasswordForm.tsx
+++ resources/src/components/organisms/forms/ForgotPasswordForm.tsx
@@ -5,7 +5,11 @@ import { FormInput } from "@/components/molecules/forms/FormInput";
 export const ForgotPasswordForm: VFC = () => {
     return (
         <form>
-            <FormInput htmlFor="email" placeholder="メールアドレスを入力">
+            <FormInput
+                htmlFor="email"
+                placeholder="メールアドレスを入力"
+                type="mail"
+            >
                 メールアドレス
             </FormInput>
         </form>
