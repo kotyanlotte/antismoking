4f4646154c373acad8f3308c0602b91315693f23
kotyan0427 <koulotte0427@outlook.jp>
2021-09-18 20:29:22 +0900

    inputのvalue値を取得する関数

--- resources/src/components/organisms/forms/ForgotPasswordForm.tsx
+++ resources/src/components/organisms/forms/ForgotPasswordForm.tsx
@@ -3,6 +3,15 @@ import React, { VFC } from "react";
 import { FormInput } from "@/components/molecules/forms/FormInput";
 
 export const ForgotPasswordForm: VFC = () => {
+    const [email, setEmail] = useRecoilState(resetLinkEmailState);
+    // パスワードリセットリンク発行のためのメールアドレスを取得する関数
+    const onChangeEmail = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            setEmail(e.target.value);
+        },
+        []
+    );
+
     return (
         <form>
             <FormInput
@@ -10,6 +19,7 @@ export const ForgotPasswordForm: VFC = () => {
                 placeholder="メールアドレスを入力"
                 type="mail"
                 value={email}
+                onChange={onChangeEmail}
             >
                 メールアドレス
             </FormInput>
