f73f1f1fb8a30f37668d82a843618c804772a18c
kotyan0427 <koulotte0427@outlook.jp>
2021-09-23 17:38:35 +0900

    ログイン後にlocationでリダイレクトを変える処理の削除

--- resources/src/components/organisms/AuthUser.tsx
+++ resources/src/components/organisms/AuthUser.tsx
@@ -1,5 +1,5 @@
 import React, { memo, VFC } from "react";
-import { Redirect, Route, useLocation } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 import { isLoggedInState } from "@/components/store/loginUserState";
@@ -12,12 +12,10 @@ type AuthUserType = {
 export const AuthUser: VFC<AuthUserType> = memo(({ path, component }) => {
     const isLoggedIn = useRecoilValue(isLoggedInState);
 
-    const location = useLocation<Location>();
-
     return isLoggedIn ? (
         <Route path={path} component={component} />
     ) : (
-        <Redirect to={{ pathname: "/login", state: { from: location } }} />
+        <Redirect to="/login" />
     );
 });
 
