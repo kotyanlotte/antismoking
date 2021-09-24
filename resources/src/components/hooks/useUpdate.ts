1a1f200359a20bc7e8aa178438b89700e69e1dff
kotyan0427 <koulotte0427@outlook.jp>
2021-09-24 10:59:02 +0900

    ユーザー情報を更新するロジックの実装

---
+++ resources/src/components/hooks/useUpdate.ts
@@ -0,0 +1,65 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useRecoilState, useSetRecoilState } from "recoil";
+
+import { isLoadingButton } from "@/components/store/buttonSpinner";
+import {
+    updateEmailErrorState,
+    updateNameErrorState,
+} from "@/components/store/userUpdateErrorState";
+import {
+    updateEmailState,
+    updateNameState,
+} from "@/components/store/userUpdateState";
+import { UpdateErrorType } from "@/components/types/updateErrorType";
+
+type UpdateReturnType = {
+    update: () => void;
+    loading: boolean;
+};
+
+export const useUpdate = (): UpdateReturnType => {
+    const [name, setName] = useRecoilState(updateNameState);
+    const [email, setEmail] = useRecoilState(updateEmailState);
+
+    const setErrorName = useSetRecoilState(updateNameErrorState);
+    const setErrorEmail = useSetRecoilState(updateEmailErrorState);
+
+    const [loading, setLoading] = useRecoilState(isLoadingButton);
+
+    const history = useHistory<History>();
+
+    const update = useCallback(async () => {
+        setLoading(true);
+        await axios
+            .put("user/profile-information", {
+                name: name,
+                email: email,
+            })
+            .then(() => {
+                setName("");
+                setEmail("");
+                setErrorName([]);
+                setErrorEmail([]);
+                setLoading(false);
+                toast.success("ユーザー情報の更新に成功しました!");
+                history.push("/home");
+            })
+            .catch((e: UpdateErrorType) => {
+                setName("");
+                setEmail("");
+                setErrorName(e.response.data.errors.name);
+                setErrorEmail(e.response.data.errors.email);
+                setLoading(false);
+                toast.error("ユーザー情報の更新に失敗しました");
+            });
+    }, [name, email]);
+
+    return {
+        update,
+        loading,
+    };
+};
--- resources/src/components/organisms/updateArea.tsx
+++ resources/src/components/organisms/updateArea.tsx
@@ -1,9 +1,12 @@
 import React, { VFC } from "react";
 
 import { PrimaryButton } from "@/components/atoms/buttons/PrimaryButton";
+import { useUpdate } from "@/components/hooks/useUpdate";
 import { UpdateForm } from "@/components/organisms/forms/UpdateForm";
 
 export const UpdateArea: VFC = () => {
+    const { update, loading } = useUpdate();
+
     return (
         <div className="bg-gray-light py-12 flex justify-center">
             <div className="card w-650 space-y-8">
@@ -11,7 +14,11 @@ export const UpdateArea: VFC = () => {
                     ユーザー情報の更新
                 </h2>
                 <UpdateForm />
-                <PrimaryButton style="text-green-default w-52 border-green-default hover:bg-green-default hover:text-white-default">
+                <PrimaryButton
+                    onClick={update}
+                    style="text-green-default w-52 border-green-default hover:bg-green-default hover:text-white-default"
+                    loading={loading}
+                >
                     更新
                 </PrimaryButton>
             </div>
