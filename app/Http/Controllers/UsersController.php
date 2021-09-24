3d4eef67ea5207bb4a9d824e5e385a9d24233d70
kotyan0427 <koulotte0427@outlook.jp>
2021-09-24 15:42:51 +0900

    アカウント削除するルーティングの設定

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+    public function destroy(){
+        $user = Auth::user();
+        dd($user);
+    }
+}
