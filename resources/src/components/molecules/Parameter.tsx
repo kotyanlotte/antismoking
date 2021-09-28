96f0cc6d81e9447256fe02d03065680075af03f9
kotyan0427 <koulotte0427@outlook.jp>
2021-09-28 12:28:33 +0900

    チャートのCSSを修正

--- resources/src/components/molecules/Parameter.tsx
+++ resources/src/components/molecules/Parameter.tsx
@@ -1,43 +1,44 @@
 import React, { VFC } from "react";
 import {
+    Legend,
     PolarAngleAxis,
     PolarGrid,
     PolarRadiusAxis,
     Radar,
     RadarChart,
     ResponsiveContainer,
+    Tooltip,
 } from "recharts";
 
 const data = [
     {
-        value: "健康値",
-        A: 100,
-        fullMark: 100,
+        title: "健康値",
+        value: 100,
     },
     {
-        value: "メンタル値",
-        A: 98,
-        fullMark: 100,
+        title: "メンタル値",
+        value: 98,
     },
     {
-        value: "脳値",
-        A: 86,
-        fullMark: 100,
+        title: "脳値",
+        value: 86,
     },
 ];
 
 export const Parameter: VFC = () => {
     return (
-        <ResponsiveContainer width={"100%"} height={300}>
-            <RadarChart outerRadius={90} data={data}>
+        <ResponsiveContainer width={"100%"} height={400}>
+            <RadarChart outerRadius={"90%"} data={data} margin={{ bottom: 60 }}>
+                <Legend verticalAlign={"top"} height={36} />
+                <Tooltip />
                 <PolarGrid />
-                <PolarAngleAxis dataKey="value" />
+                <PolarAngleAxis dataKey="title" />
                 <PolarRadiusAxis angle={30} domain={[0, 100]} />
                 <Radar
                     name="Lily"
-                    dataKey="A"
-                    stroke="#82ca9d"
-                    fill="#82ca9d"
+                    dataKey="value"
+                    stroke="#6EE7B7"
+                    fill="#6EE7B7"
                     fillOpacity={0.6}
                 />
             </RadarChart>
