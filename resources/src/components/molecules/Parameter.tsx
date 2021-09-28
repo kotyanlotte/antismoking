6c1b75f3284406bc197fd4a00ce2103ac4e420d7
kotyan0427 <koulotte0427@outlook.jp>
2021-09-28 15:47:53 +0900

    チャートに表示するデータをapiから取得したデータに変える

--- resources/src/components/molecules/Parameter.tsx
+++ resources/src/components/molecules/Parameter.tsx
@@ -9,23 +9,28 @@ import {
     ResponsiveContainer,
     Tooltip,
 } from "recharts";
+import { useRecoilValue } from "recoil";
 
-const data = [
-    {
-        title: "健康値",
-        value: 100,
-    },
-    {
-        title: "メンタル値",
-        value: 98,
-    },
-    {
-        title: "脳値",
-        value: 86,
-    },
-];
+import { userState } from "@/components/store/userState";
 
 export const Parameter: VFC = () => {
+    const value = useRecoilValue(userState);
+
+    const data = [
+        {
+            title: "健康値",
+            value: value?.health_value,
+        },
+        {
+            title: "メンタル値",
+            value: value?.mental_value,
+        },
+        {
+            title: "脳値",
+            value: value?.brain_value,
+        },
+    ];
+
     return (
         <ResponsiveContainer width={"100%"} height={400}>
             <RadarChart outerRadius={"90%"} data={data} margin={{ bottom: 60 }}>
@@ -35,7 +40,7 @@ export const Parameter: VFC = () => {
                 <PolarAngleAxis dataKey="title" />
                 <PolarRadiusAxis angle={30} domain={[0, 100]} />
                 <Radar
-                    name="Lily"
+                    name={`${value?.name}さん`}
                     dataKey="value"
                     stroke="#6EE7B7"
                     fill="#6EE7B7"
