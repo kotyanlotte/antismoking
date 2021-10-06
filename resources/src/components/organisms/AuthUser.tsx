c28684266222a8e285f907d5cfc991047d0e1cf5
kotyan0427 <koulotte0427@outlook.jp>
2021-10-06 16:42:10 +0900

    ゲストログインユーザーのルーティングの制御

--- resources/src/components/organisms/AuthUser.tsx
+++ resources/src/components/organisms/AuthUser.tsx
@@ -1,7 +1,9 @@
 import React, { memo, VFC } from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route, useLocation } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
+import { Home } from "@/components/pages/Home";
+import { isLoggedInGuestState } from "@/components/store/login/loginGuestUserState";
 import { isLoggedInState } from "@/components/store/login/loginUserState";
 
 type AuthUserType = {
@@ -11,8 +13,14 @@ type AuthUserType = {
 
 export const AuthUser: VFC<AuthUserType> = memo(({ path, component }) => {
     const isLoggedIn = useRecoilValue(isLoggedInState);
+    const isLoggedInGuest = useRecoilValue(isLoggedInGuestState);
 
-    return isLoggedIn ? (
+    return isLoggedInGuest ? (
+        <>
+            <Redirect from="/edit" to="/home" />
+            <Route path="/home" component={Home} />
+        </>
+    ) : isLoggedIn ? (
         <Route path={path} component={component} />
     ) : (
         <Redirect to="/login" />
