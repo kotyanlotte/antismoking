d4fdc51f1a0af899c4809ad742212e6759b4444a
kotyan0427 <koulotte0427@outlook.jp>
2021-10-06 17:10:40 +0900

    404ページのための画像の用意

--- resources/src/components/pages/NoMatch.tsx
+++ resources/src/components/pages/NoMatch.tsx
@@ -1,5 +1,19 @@
 import React, { VFC } from "react";
 
 export const NoMatch: VFC = () => {
-    return <h1>404ページです</h1>;
+    return (
+        <div>
+            <picture>
+                <source
+                    srcSet="images/404page/undraw_page_not_found.webp"
+                    type="image/webp"
+                />
+                <img
+                    className=""
+                    src="/images/404page/undraw_page_not_found.jpg"
+                    alt="page not found"
+                />
+            </picture>
+        </div>
+    );
 };
