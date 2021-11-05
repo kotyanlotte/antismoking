a2a7de35a2298472b5e8ca8e75eb368cff15fb26
kotyan0427 <koulotte0427@outlook.jp>
2021-11-05 17:48:34 +0900

    lazyとsuspenseとrouterを使用したパフォーマンスの改善

--- resources/src/components/router/Routing.tsx
+++ resources/src/components/router/Routing.tsx
@@ -1,7 +1,7 @@
 import "react-toastify/dist/ReactToastify.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-import React, { VFC } from "react";
+import React, { lazy, Suspense, VFC } from "react";
 import ReactDOM from "react-dom";
 import {
     BrowserRouter as Router,
@@ -12,51 +12,100 @@ import {
 import { ToastContainer } from "react-toastify";
 import { RecoilRoot } from "recoil";
 
-import { AuthUser } from "@/components/organisms/AuthUser";
-import { NotLoggedInUser } from "@/components/organisms/NotLoggedInUser";
-import { About } from "@/components/pages/About";
-import { Edit } from "@/components/pages/Edit";
-import { ForgotPassword } from "@/components/pages/ForgotPassword";
-import { Home } from "@/components/pages/Home";
-import { Login } from "@/components/pages/Login";
-import { NoMatch } from "@/components/pages/NoMatch";
-import { Policy } from "@/components/pages/Policy";
-import { Register } from "@/components/pages/Register";
-import { ResetPassword } from "@/components/pages/ResetPassword";
-import { Terms } from "@/components/pages/Terms";
-import { ScrollToTop } from "@/components/utils/ScrollToTop";
+import { Loading } from "@/components/molecules/Loading";
+
+const AuthUser = lazy(() =>
+    import("@/components/organisms/AuthUser").then(({ AuthUser }) => ({
+        default: AuthUser,
+    }))
+);
+const NotLoggedInUser = lazy(() =>
+    import("@/components/organisms/NotLoggedInUser").then(
+        ({ NotLoggedInUser }) => ({ default: NotLoggedInUser })
+    )
+);
+const About = lazy(() =>
+    import("@/components/pages/About").then(({ About }) => ({ default: About }))
+);
+const Edit = lazy(() =>
+    import("@/components/pages/Edit").then(({ Edit }) => ({ default: Edit }))
+);
+const ForgotPassword = lazy(() =>
+    import("@/components/pages/ForgotPassword").then(({ ForgotPassword }) => ({
+        default: ForgotPassword,
+    }))
+);
+const Home = lazy(() =>
+    import("@/components/pages/Home").then(({ Home }) => ({ default: Home }))
+);
+const Login = lazy(() =>
+    import("@/components/pages/Login").then(({ Login }) => ({ default: Login }))
+);
+const NoMatch = lazy(() =>
+    import("@/components/pages/NoMatch").then(({ NoMatch }) => ({
+        default: NoMatch,
+    }))
+);
+const Policy = lazy(() =>
+    import("@/components/pages/Policy").then(({ Policy }) => ({
+        default: Policy,
+    }))
+);
+const Register = lazy(() =>
+    import("@/components/pages/Register").then(({ Register }) => ({
+        default: Register,
+    }))
+);
+const ResetPassword = lazy(() =>
+    import("@/components/pages/ResetPassword").then(({ ResetPassword }) => ({
+        default: ResetPassword,
+    }))
+);
+const Terms = lazy(() =>
+    import("@/components/pages/Terms").then(({ Terms }) => ({ default: Terms }))
+);
+const ScrollToTop = lazy(() =>
+    import("@/components/utils/ScrollToTop").then(({ ScrollToTop }) => ({
+        default: ScrollToTop,
+    }))
+);
 
 export const Routing: VFC = () => {
     return (
         <Router>
-            <RecoilRoot>
-                <ScrollToTop />
-                <Switch>
-                    <Redirect exact from="/" to="/about" />
-                    <Route path="/about" component={About} />
-                    <Route path="/terms" component={Terms} />
-                    <Route path="/policy" component={Policy} />
-                    <Route
-                        path="/reset-password/:token"
-                        component={ResetPassword}
-                    />
+            <Suspense fallback={<Loading />}>
+                <RecoilRoot>
+                    <ScrollToTop />
+                    <Switch>
+                        <Redirect exact from="/" to="/about" />
+                        <Route path="/about" component={About} />
+                        <Route path="/terms" component={Terms} />
+                        <Route path="/policy" component={Policy} />
+                        <Route
+                            path="/reset-password/:token"
+                            component={ResetPassword}
+                        />
 
-                    {/* 未ログインでアクセス可能 */}
-                    <NotLoggedInUser path="/login" component={Login} />
-                    <NotLoggedInUser path="/register" component={Register} />
-                    <NotLoggedInUser
-                        path="/forgot-password"
-                        component={ForgotPassword}
-                    />
+                        {/* 未ログインでアクセス可能 */}
+                        <NotLoggedInUser path="/login" component={Login} />
+                        <NotLoggedInUser
+                            path="/register"
+                            component={Register}
+                        />
+                        <NotLoggedInUser
+                            path="/forgot-password"
+                            component={ForgotPassword}
+                        />
 
-                    {/* ログイン済みでアクセス可能 */}
-                    <AuthUser path="/home" component={Home} />
-                    <AuthUser path="/edit" component={Edit} />
+                        {/* ログイン済みでアクセス可能 */}
+                        <AuthUser path="/home" component={Home} />
+                        <AuthUser path="/edit" component={Edit} />
 
-                    <Route path="*" component={NoMatch} />
-                </Switch>
-                <ToastContainer position="top-center" autoClose={3000} />
-            </RecoilRoot>
+                        <Route path="*" component={NoMatch} />
+                    </Switch>
+                    <ToastContainer position="top-center" autoClose={3000} />
+                </RecoilRoot>
+            </Suspense>
         </Router>
     );
 };
