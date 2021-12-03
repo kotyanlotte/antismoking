74c47a6baf97107507d0b62975930a68edfee04c
kotyan0427 <koulotte0427@outlook.jp>
2021-12-03 18:11:13 +0900

    labelをクリックしたらフォーカスがinputに移るようにする

--- resources/src/components/organisms/CigarettesArea.tsx
+++ resources/src/components/organisms/CigarettesArea.tsx
@@ -5,9 +5,12 @@ import { CigarettesForm } from "@/components/molecules/forms/CigarettesForm";
 export const CigarettesArea: VFC = () => {
     return (
         <div className="p-10">
-            <h2 className="font-bold text-xl mb-5 surface:text-center">
+            <label
+                className="font-bold text-xl mb-5 surface:text-center block"
+                htmlFor="cigarettes"
+            >
                 吸ったタバコの本数を入力してください。
-            </h2>
+            </label>
             <CigarettesForm />
         </div>
     );
